d43caa0e2081250d491c941e920285ea505a8d46
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-27 22:56:27 +0700

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -81,7 +81,7 @@ const RevenueByMonthsChart = () => {
     useEffect (()=>{
         Table() 
     },[])
-    const endpoint = 'http://hotelroyal.great-site.net/api/transactions'
+    const endpoint = 'https://hotelroyal.great-site.net/api/transactions'
     const [ListBookings,setListBooking]=useState([]);
     const Table = () => {
         axios.get(endpoint).then((Response)=>{
