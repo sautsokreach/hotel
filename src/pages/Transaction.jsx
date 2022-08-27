05a5237adf597aec0f427a2c739e8745c96f748c
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-27 22:57:04 +0700

    Update Transaction.jsx

--- src/pages/Transaction.jsx
+++ src/pages/Transaction.jsx
@@ -49,7 +49,7 @@ const Rooms = () => {
         Table() 
     },[])
     const [ListRoomss,setListRooms]=useState([]);
-    const endpoint = 'http://hotelroyal.great-site.net/api/transactions'
+    const endpoint = 'https://hotelroyal.great-site.net/api/transactions'
     const Table = () => {
         axios.get(endpoint).then((Response)=>{
             var data1 = Response.data
