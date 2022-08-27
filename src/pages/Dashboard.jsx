1350c6022c565e3931b4251c08cf52c6372c2489
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-27 10:06:34 +0700

    Add files via upload

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,154 @@
+import {React,useState,useEffect} from 'react'
+import { Bar } from 'react-chartjs-2'
+import Box from '../components/box/Box'
+import DashboardWrapper, { DashboardWrapperMain, DashboardWrapperRight } from '../components/dashboard-wrapper/DashboardWrapper'
+import SummaryBox, { SummaryBoxSpecial } from '../components/summary-box/SummaryBox'
+import { colors, data } from '../constants'
+import axios from 'axios';
+
+
+
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+} from 'chart.js'
+import OverallList from '../components/overall-list/OverallList'
+import RevenueList from '../components/revenue-list/RevenueList'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+)
+
+const Dashboard = () => {
+   
+    return (
+        <DashboardWrapper>
+            <DashboardWrapperMain>
+                {/* <div className="row">
+                    <div className="col-8 col-md-12">
+                        <div className="row">
+                            {
+                                data.summary.map((item, index) => (
+                                    <div key={`summary-${index}`} className="col-6 col-md-6 col-sm-12 mb">
+                                        <SummaryBox item={item} />
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className="col-4 hide-md">
+                        <SummaryBoxSpecial item={data.revenueSummary} />
+                    </div>
+                </div> */}
+                <div className="row">
+                    <div className="col-12">
+                        <Box>
+                            <RevenueByMonthsChart />
+                        </Box>
+                    </div>
+                </div>
+            </DashboardWrapperMain>
+            {/* <DashboardWrapperRight>
+                <div className="title mb">Overall</div>
+                <div className="mb">
+                    <OverallList />
+                </div>
+                <div className="title mb">Revenue by channel</div>
+                <div className="mb">
+                    <RevenueList />
+                </div>
+            </DashboardWrapperRight> */}
+        </DashboardWrapper>
+    )
+}
+
+export default Dashboard
+
+const RevenueByMonthsChart = () => {
+    var data2
+    useEffect (()=>{
+        Table() 
+    },[])
+    const endpoint = 'http://127.0.0.1:3001/api/transactions'
+    const [ListBookings,setListBooking]=useState([]);
+    const Table = () => {
+        axios.get(endpoint).then((Response)=>{
+            var data1 = Response.data
+            data2 = [0,0,0,0,0,0,0,0,0,0,0,0];
+            if(data1){
+            data1.map((object,item)=>{
+                var month = new Date(object.start_date);
+                data2[month.getMonth()] += parseFloat(object.price);
+            })
+            console.log(data2)
+            setListBooking(data2)
+            } 
+        })
+    } 
+
+    const chartOptions = {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+            xAxes: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            },
+            yAxes: {
+                grid: {
+                    display: false,
+                    drawBorder: false
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            },
+            title: {
+                display: false
+            }
+        },
+        elements: {
+            bar: {
+                backgroundColor: colors.orange,
+                borderRadius: 20,
+                borderSkipped: 'bottom'
+            }
+        }
+    }
+
+    const chartData = {
+        labels: data.revenueByMonths.labels,
+        datasets: [
+            {
+                label: 'Revenue',
+                data: ListBookings
+            }
+        ]
+    }
+    return (
+        <>
+            <div className="title mb">
+                Profit by months
+            </div>
+            <div>
+                <Bar options={chartOptions} data={chartData} height={`300px`} />
+            </div>
+        </>
+    )
+}
---
+++ src/pages/Transaction.jsx
@@ -0,0 +1,105 @@
+import {React,useState,useEffect} from 'react'
+import MaterialTable from 'material-table'
+import axios from 'axios';
+import { forwardRef } from 'react';
+import AddBox from '@material-ui/icons/AddBox';
+import ArrowUpward from '@material-ui/icons/ArrowUpward';
+import Check from '@material-ui/icons/Check';
+import ChevronLeft from '@material-ui/icons/ChevronLeft';
+import ChevronRight from '@material-ui/icons/ChevronRight';
+import Clear from '@material-ui/icons/Clear';
+import DeleteOutline from '@material-ui/icons/DeleteOutline';
+import Edit from '@material-ui/icons/Edit';
+import FilterList from '@material-ui/icons/FilterList';
+import FirstPage from '@material-ui/icons/FirstPage';
+import LastPage from '@material-ui/icons/LastPage';
+import Remove from '@material-ui/icons/Remove';
+import SaveAlt from '@material-ui/icons/SaveAlt';
+import Search from '@material-ui/icons/Search';
+import ViewColumn from '@material-ui/icons/ViewColumn';
+import AddIcon from '@material-ui/icons/Add';
+import GetAppIcon from '@material-ui/icons/GetApp';
+
+
+const tableIcons = {
+Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
+Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
+Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
+DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
+Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
+Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
+FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
+LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
+NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
+PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
+ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
+Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
+SortArrow: forwardRef((props, ref) => <ArrowUpward {...props} ref={ref} />),
+ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
+ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
+};
+
+const Rooms = () => {
+
+    const columns = [{title:'Room',field:'room'},{title:'Customer',field:'customer'},{title:'Telephone',field:'telephone'},{title:'Start',field:'start'},{title:'End',field:'end'},{title:'ID Card',field:'id_card'},{title:'Price',field:'price'}]
+
+    useEffect (()=>{
+        Table() 
+    },[])
+    const [ListRoomss,setListRooms]=useState([]);
+    const endpoint = 'http://127.0.0.1:3001/api/transactions'
+    const Table = () => {
+        axios.get(endpoint).then((Response)=>{
+            var data1 = Response.data
+            var data = [];
+            if(data1){
+            data1.map((object,item)=>{
+                data.push({room:object.room_no,customer:object.customer_name,telephone:object.telephone,start:object.start_date,end:object.end_date,id_card:object.id_card,id:object.id,price:object.price});
+            })
+            }
+            setListRooms(data);
+        })
+    }
+    return ( 
+        <MaterialTable columns={columns}
+  title="Rooms List"
+  icons={tableIcons}
+  data={ListRoomss}
+  editable={{
+    // onRowUpdate: (newRow, oldRow) => new Promise((resolve, reject) => {
+    //   axios.put(endpoint,newRow).then(function (response) {
+    //     console.log(response)
+
+    //     Table()
+    //   })
+    //   setTimeout(() => resolve(), 500)
+    // }),
+    // onRowDelete: (selectedRow) => new Promise((resolve, reject) => {
+    //     axios.delete(endpoint,{data:selectedRow}).then(function (response) {
+    //         Table()
+    //       })
+    //   setTimeout(() => resolve(), 1000)
+    // })
+  }}
+        onSelectionChange={(selectedRows) => console.log(selectedRows)}
+        options={{
+          paging: false,
+          sorting: true, search: true,
+          paginationType: "stepped", showFirstLastPageButtons: false, paginationPosition: "both", exportButton: true,
+          exportAllData: true, exportFileName: "TableData", addRowPosition: "first", actionsColumnIndex: -1, selection: true,
+          showSelectAllCheckbox: false, showTextRowsSelected: true, selectionProps: rowData => ({
+            color:"primary"
+          }),
+          grouping: true, columnsButton: true,
+          rowStyle: (data, index) => index % 2 === 0 ? { background: "#f5f5f5" } : null,
+          headerStyle: { background: "#f44336",color:"#fff"}
+        }}
+
+/>
+    )
+}
+
+export default Rooms
+ 
