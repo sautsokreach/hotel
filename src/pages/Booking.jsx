068a4a406f35d74df2fec3b72656b5009c34b11b
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-27 22:55:59 +0700

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -49,7 +49,7 @@ const Booking = () => {
         Table() 
     },[])
     const [ListBookings,setListBooking]=useState([]);
-    const endpoint = 'http://hotelroyal.great-site.net/api/Bookings'
+    const endpoint = 'https://hotelroyal.great-site.net/api/Bookings'
     const Table = () => {
         axios.get(endpoint).then((Response)=>{
             var data1 = Response.data
