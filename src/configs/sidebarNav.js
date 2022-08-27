11ee88e7bc399f1d8539a624ff93dc28bf9cfe20
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-27 10:08:28 +0700

    Add files via upload

---
+++ src/configs/sidebarNav.js
@@ -0,0 +1,40 @@
+const sidebarNav = [
+    {
+        link: '/',
+        section: 'home',
+        icon: <i className='bx bx-home-alt'></i>,
+        text: 'Home'
+    },
+    {
+        link: '/rooms',
+        section: 'rooms',
+        icon: <i className='bx bx-receipt' ></i>,
+        text: 'Rooms'
+    },
+    {
+        link: '/booking',
+        section: 'Booking',
+        icon: <i className='bx bx-cube'></i>,
+        text: 'Bookings'
+    },
+    {
+        link: '/addroom',
+        section: 'Addroom',
+        icon: <i className='bx bx-user'></i>,
+        text: 'AddRoom'
+    },
+    {
+        link: '/transaction',
+        section: 'transaction',
+        icon: <i className='bx bx-line-chart'></i>,
+        text: 'Transactions'
+    },
+    {
+        link: '/settings',
+        section: 'settings',
+        icon: <i className='bx bx-cog'></i>,
+        text: 'Settings'
+    }
+]
+
+export default sidebarNav
