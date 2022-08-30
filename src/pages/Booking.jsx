6ddcee62f5f0643eca41e2559c0d40ddc0ae5c6e
sautsokreach <108916985+sautsokreach@users.noreply.github.com>
2022-08-30 12:10:53 +0700

    Update Booking.jsx

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -49,7 +49,7 @@ const Booking = () => {
         Table() 
     },[])
     const [ListBookings,setListBooking]=useState([]);
-    const endpoint = https://druidical-hair.000webhostapp.com/api/Bookings'
+    const endpoint = 'https://druidical-hair.000webhostapp.com/api/Bookings'
     const Table = () => {
         axios.get(endpoint).then((Response)=>{
             var data1 = Response.data
